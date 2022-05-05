ec9900ab704dfb5655f70ef901f1483003c51c90
Priyadarshinimathivanan0101 <Priya6382709056@gmail.com>
2022-05-05 15:17:55 +0530

    stringOperations
    05.05.2022

---
+++ app/src/main/java/com/example/practice/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.practice
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+        private lateinit var reverseButton: Button
+        private lateinit var splitButton: Button
+        private lateinit var appendButton: Button
+        private lateinit var resultText: TextView
+        private lateinit var result: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        appendButton = findViewById<Button>(R.id.appendButton)
+        reverseButton = findViewById<Button>(R.id.reverseButton)
+        splitButton = findViewById<Button>(R.id.splitButton)
+        resultText = findViewById<TextView>(R.id.resultText)
+        result = findViewById<TextView>(R.id.result)
+        appendButton.setOnClickListener {
+            var intent = Intent(this, MainActivity2::class.java)
+            intent.putExtra("operation", "append")
+            startActivityForResult(intent,0)
+        }
+        reverseButton.setOnClickListener {
+            var intent = Intent(this, MainActivity2::class.java)
+            intent.putExtra("operation", "reverse")
+            startActivityForResult(intent,0)
+        }
+        splitButton.setOnClickListener {
+            var intent = Intent(this, MainActivity2::class.java)
+            intent.putExtra("operation", "split")
+            startActivityForResult(intent,0)
+        }
+    }
+   override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+       appendButton = findViewById<Button>(R.id.appendButton)
+       reverseButton = findViewById<Button>(R.id.reverseButton)
+       splitButton = findViewById<Button>(R.id.splitButton)
+       resultText = findViewById<TextView>(R.id.resultText)
+       result = findViewById<TextView>(R.id.result)
+       appendButton.visibility = View.GONE
+       reverseButton.visibility = View.GONE
+       splitButton.visibility = View.GONE
+       resultText.visibility = View.VISIBLE
+       result.visibility = View.VISIBLE
+        if(requestCode == 0) {
+            result.text = data!!.getStringExtra("data")
+        }
+    }
+}
---
+++ app/src/main/java/com/example/practice/MainActivity2.kt
@@ -0,0 +1,47 @@
+package com.example.practice
+
+import android.content.Intent
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity2 : AppCompatActivity() {
+    private lateinit var string1: EditText
+    private lateinit var string2: EditText
+    private lateinit var operationButton : Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        val operation = intent.getStringExtra("operation")
+        operationButton = findViewById<Button>(R.id.doneButton)
+        operationButton.text = operation
+        string1 = findViewById<EditText>(R.id.string1)
+        string2 = findViewById<EditText>(R.id.string2)
+        if(operation.equals("append"))
+            string2.visibility = View.VISIBLE
+        operationButton.setOnClickListener {
+            when (operation.toString()) {
+                "append" -> {
+                    sendToActivity1("${string1.text} ${string2.text}")
+                }
+                "reverse" -> {
+                    sendToActivity1((string1.text).toString().reversed())
+                }
+                "split" -> {
+                    val chars: List<Char> = string1.text.toList()
+                    sendToActivity1(chars.joinToString("  "))
+                }
+            }
+        }
+    }
+    fun sendToActivity1(output: String) {
+        var intent = Intent().putExtra("data", output.toString())
+        setResult(0, intent)
+        finish()
+    }
+}
